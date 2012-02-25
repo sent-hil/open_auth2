fa91193814044747b91bd82ff9d0f758dca4a84e
Senthil A <senthil196@gmail.com>
2012-02-25 15:17:51 -0800

    minor comment fix

--- lib/open_auth2/client.rb
+++ lib/open_auth2/client.rb
@@ -1,6 +1,6 @@
 module OpenAuth2
 
-  # Used to make get/post requests to OAuth server.
+  # Used to make GET/POST requests to OAuth server.
   class Client
     extend DelegateToConfig
     include Connection
