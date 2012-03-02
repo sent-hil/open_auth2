8bceb00821fd568c8f9e8e2aaf9e9c65d991d74d
Senthil A <senthil196@gmail.com>
2012-03-02 15:09:01 -0800

    added #get, #post, #run_request sections for Google example

--- examples/google.rb
+++ examples/google.rb
@@ -1,13 +1,18 @@
 require_relative '../lib/open_auth2'
 require 'json'
 
-@access_token  = '77848792642.apps.googleusercontent.com'
-@refresh_token = 'ZtXPd6DkYYhIbmRQxwLaDUqp'
+@code          = ''
+@client_id     = ''
+@client_secret = ''
+@access_token  = ''
+@post_email    = ''
 
 @config = OpenAuth2::Config.new do |c|
   c.provider       = :google
+  c.code           = @code
+  c.client_id      = @client_id
+  c.client_secret  = @client_secret
   c.access_token   = @access_token
-  c.refresh_token  = @refresh_token
   c.scope          = ['https://www.googleapis.com/auth/calendar']
   c.redirect_uri   = 'http://localhost:9393/google/callback'
   c.path_prefix    = '/calendar/v3'
@@ -17,5 +22,29 @@ end
   c.config = @config
 end
 
+@token = @client.token
+
 # get request
-@p = proc { @client.get(:path => '/users/me/calendarList') }
+#@list = @client.get(:path => '/users/me/calendarList')
+
+post_url = "/calendar/v3/calendars/#{post_email}/events"
+body     = {
+  "summary" => "From OpenAuth2",
+  "start"   => {"dateTime"=>"2012-03-03T10:00:00.000-07:00"},
+  "end"     => {"dateTime"=>"2012-03-03T10:25:00.000-07:00"}
+}
+body = JSON.dump(body)
+
+# post request
+@client.post(:path         => post_url,
+             :body         => body,
+             :content_type => "application/json")
+
+header   = {"Content-Type" => "application/json"}
+full_url = "#{post_url}?access_token=#{@access_token}"
+
+# post request via #run_request
+@client.run_request(:verb   => :post,
+                    :path   => full_url,
+                    :body   => body,
+                    :header => header)
