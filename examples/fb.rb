e256e42d4a2530ce97d9b062d4ec9ff3861cb00d
sent-hil <me@sent-hil.com>
2012-09-28 22:23:19 -0700

    Release 0.0.5

--- examples/fb.rb
+++ examples/fb.rb
@@ -1,9 +1,20 @@
 require_relative '../lib/open_auth2'
 
+ClientId = '216091171753850'
+ClientSecret = '56f751bef00d6d21e858566b873ac8f8'
+Code = ''
+AccessToken = ''
+
 @config = OpenAuth2::Config.new do |c|
-  c.provider = :facebook
+  c.provider       = :facebook
+  c.client_id      = ClientId
+  c.client_secret  = ClientSecret
+  c.code           = Code
+  c.access_token   = AccessToken
+  c.redirect_uri   = 'http://localhost:9393/'
+  c.scope          = ['manage_pages', 'read_insights']
 end
 
 @client = OpenAuth2::Client.new(@config)
-
-@coke = @client.get(:path => '/cocacola')
+@url    = @client.build_code_url
+@token  = @client.token
--- examples/google.rb
+++ examples/google.rb
@@ -1,18 +1,20 @@
 require_relative '../lib/open_auth2'
 require 'json'
 
-ClientId     = nil
-ClientSecret = nil
+ClientId     = '948773240950.apps.googleusercontent.com'
+ClientSecret = 'MTkeuqOhFTbfRbd27BcEROTl'
 Code         = nil
-AccessToken  = nil
-RefreshToken = nil
-PostEmail    = nil
+AccessToken  = 'ya29.AHES6ZRL6dKYn5HvssNQvH15KXTc76jCd9KC6Wfsir74whQ'
+RefreshToken = '1/2hTXHN9FULj7v_hVOIoyHn6BpOQS6uDOw-xllInXnTU'
+PostEmail    = 'senthil196@gmail.com'
 
 @config = OpenAuth2::Config.new do |c|
   c.provider       = :google
   c.code           = Code
   c.client_id      = ClientId
   c.client_secret  = ClientSecret
+  c.access_token   = AccessToken
+  c.refresh_token  = RefreshToken
   c.scope          = ['https://www.googleapis.com/auth/calendar']
   c.redirect_uri   = 'http://localhost:9393/google/callback'
   c.path_prefix    = '/calendar/v3'
@@ -22,32 +24,34 @@ end
   c.config = @config
 end
 
-@token = @client.token
+#@token = @client.token
 
-params = {:approval_prompt => 'force', :access_type => 'offline'}
-@url  = @token.build_code_url(params)
+#params = {:approval_prompt => 'force', :access_type => 'offline'}
+#@url  = @token.build_code_url(params)
 
 # get request
 @list = @client.get(:path => '/users/me/calendarList')
 
-post_url = "/calendar/v3/calendars/#{PostEmail}/events"
-body     = {
+@post_url = "/calendar/v3/calendars/#{PostEmail}/events"
+@body     = {
   "summary" => "From OpenAuth2",
-  "start"   => {"dateTime"=>"2012-03-03T10:00:00.000-07:00"},
-  "end"     => {"dateTime"=>"2012-03-03T10:25:00.000-07:00"}
+  "start"   => {"dateTime"=>"2012-10-03T10:00:00.000-07:00"},
+  "end"     => {"dateTime"=>"2012-10-03T10:25:00.000-07:00"}
 }
-body = JSON.dump(body)
+@body = JSON.dump(@body)
 
 # post request
-@client.post(:path         => post_url,
-             :body         => body,
-             :content_type => "application/json")
+def make_request
+  @response  = @client.post(:path         => @post_url,
+                            :body         => @body,
+                            :content_type => "application/json")
+end
 
-header   = {"Content-Type" => "application/json"}
-full_url = "#{post_url}?access_token=#{AccessToken}"
+#header   = {"Content-Type" => "application/json"}
+#full_url = "#{post_url}?access_token=#{AccessToken}"
 
 # post request via #run_request
-@client.run_request(:verb   => :post,
-                    :path   => full_url,
-                    :body   => body,
-                    :header => header)
+#@client.run_request(:verb   => :post,
+                    #:path   => full_url,
+                    #:body   => body,
+                    #:header => header)
