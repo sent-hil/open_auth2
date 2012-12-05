37d37df6b8c389f3834fad12d8bab2ec6455a213
sent-hil <me@sent-hil.com>
2012-12-05 12:50:47 -0500

    updated fb example

--- examples/fb.rb
+++ examples/fb.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/open_auth2'
 
-ClientId = '216091171753850'
-ClientSecret = '56f751bef00d6d21e858566b873ac8f8'
-Code = ''
-AccessToken = ''
+ClientId     = '369754433115833'
+ClientSecret = '117d2e4932154c8c408d3d170c81c2dc'
+Code         = ''
+AccessToken  = ''
 
 @config = OpenAuth2::Config.new do |c|
   c.provider       = :facebook
@@ -12,7 +12,7 @@ AccessToken = ''
   c.code           = Code
   c.access_token   = AccessToken
   c.redirect_uri   = 'http://localhost:9393/'
-  c.scope          = ['manage_pages', 'read_insights']
+  c.scope          = ['publish_stream']
 end
 
 @client = OpenAuth2::Client.new(@config)
